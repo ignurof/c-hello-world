1b0d8b539a696b0aff5488e145b687c81c5f6f20
ignurof <ignurof@outlook.com>
2022-03-11 23:30:56 +0100

    init

---
+++ Program.c
@@ -0,0 +1,12 @@
+// Library headers required for standard C functions
+#include <stdio.h>
+
+
+// Main entry point of all C programs
+int main() {
+	// Display output from standard input/output header function printf()
+	printf("Hello, World!");
+
+	// Exit status: Success
+	return 0;
+}
